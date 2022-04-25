4c59936ebbf3a6ee03077bee3fc624df752a6dab
jonas <jonazas2@gmail.com>
2022-04-25 15:08:20 +0200

    -

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace student_project
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
